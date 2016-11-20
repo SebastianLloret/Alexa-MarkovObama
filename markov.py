7887cfa1059c16913e129f42b9c547927d00beaa
Sebastian Lloret <sebastian@lloret.io>
2016-11-19 20:55:52 -0700

    Updates

--- markov.py
+++ markov.py
@@ -1,37 +1,45 @@
-import requests
 
-def build_speechlet_response(title, output, should_end_session):
+import requests
+session_attributes = {}
+
+def build_speechlet_response(title, output, speech, reprompt_text, should_end_session):
     return {
-        'version': "1.0",
-        'response': {
-            'outputSpeech': {
-                'type': 'PlainText',
-                'text': output
-            }
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': output
         },
         'card': {
             'type': 'Simple',
-            'title': "SessionSpeechlet - " + title,
-            'content': "SessionSpeechlet - " + output
+            'title': title,
+            'content': "User >> " + speech + "\nResponse >> " + output
         },
         'reprompt': {
             'outputSpeech': {
                 'type': 'PlainText',
-                'text': "I'm sorry, I didn't catch that."
+                'text': reprompt_text
             }
         },
         'shouldEndSession': should_end_session
     }
 
+def build_response(session_attributes, speechlet_response):
+    return {
+        'version': '1.0',
+        'sessionAttributes': session_attributes,
+        'response': speechlet_response
+    }
+
 def lambda_handler(event, context):
     if event["request"]["intent"]["name"] == "GetMarkovIntent":
-        if "value" not in event["request"]["intent"]["slots"]["issue"]:
-            markov = requests.get('http://talk-to-obama.herokuapp.com/chat')
+        if "value" in event["request"]["intent"]["slots"]["issue"]:
+            markov = requests.get('http://talk-to-obama.herokuapp.com/chat?size=tweet&issue=' + event["request"]["intent"]["slots"]["issue"]["value"])
             markovDictionary = markov.json()
-            response = " Quote: " + markovDictionary["content"] + " endquote."
-            return build_speechlet_response("", response, True)
+            response = markovDictionary["prefacingText"] + " Quote: " + markovDictionary["content"]
+            speech = event["request"]["intent"]["slots"]["issue"]["value"]
+            return build_response(session_attributes, build_speechlet_response("Log:", response, "Topic: " + speech, "Ask me what Obama's stance is on a topic.", True))
+
         else:
-            markov = requests.get('http://talk-to-obama.herokuapp.com/chat?issue=' + event["request"]["intent"]["slots"]["issue"]["value"])
+            markov = requests.get('http://talk-to-obama.herokuapp.com/chat?size=tweet')
             markovDictionary = markov.json()
-            response = markovDictionary["prefacingText"] + " Quote: " + markovDictionary["content"] + " endquote."
-            return build_speechlet_response("", response, True)
+            response = "Quote: " + markovDictionary["content"]
+            return build_response(session_attributes, build_speechlet_response("Log:", response, "Topic: N/A", "Ask me what Obama's stance is on a topic.", True))
